60822e4a7f533860ba4ec79092c388a9cd131dce
redanyelden <58820078+redanyelden@users.noreply.github.com>
2020-09-04 16:03:44 +0100

    Update test_controlled_terms.py

--- tableinspector/test_controlled_terms.py
+++ tableinspector/test_controlled_terms.py
@@ -13,12 +13,5 @@ class TestControlledTerms(unittest.TestCase):
     def test_check_combination(self):
         pass
 
-    def test_generate_columns(self):
-        pass
-
-    def test_find_controlled_terms(self):
-        pass
-
-
 if __name__  == '__main__':
     unittest.main()

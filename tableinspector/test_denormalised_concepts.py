7a48f69dff2a4484a060d699eb1211e16171e8cf
redanyelden <58820078+redanyelden@users.noreply.github.com>
2020-09-04 16:04:02 +0100

    Update test_denormalised_concepts.py

--- tableinspector/test_denormalised_concepts.py
+++ tableinspector/test_denormalised_concepts.py
@@ -7,16 +7,26 @@ import numpy as np
 from itertools import combinations
 import utils
 from collections import defaultdict
+import table
 import unittest
 
 class TestDenormalisedConcepts(unittest.TestCase):
 
     def test_check_combination(self):
-        pass
+        current_location = str(os.getcwd())
+        test_file_location = current_location + "\\test_data\\test_csv_8.csv"
+        
+        tbl = table.Table(test_file_location)
+        combination_size = 2
+        combinations_list = utils.generate_combinations(tbl.df.columns, combination_size)
+        combinations_with_values = utils.find_combination_values(combinations_list, tbl, combination_size)
 
-    def test_find_denormalised_concepts(self):
-        pass
-
+        test_combination = ('column1', 'column2')
+        self.assertTrue(denormalised_concepts.check_combination(combinations_with_values[test_combination]))
+        test_combination = ('column2', 'column3')
+        self.assertTrue(denormalised_concepts.check_combination(combinations_with_values[test_combination]))
+        test_combination = ('column1', 'column3')
+        self.assertTrue(denormalised_concepts.check_combination(combinations_with_values[test_combination]))
 
 if __name__  == '__main__':
     unittest.main()
